3a8dfdffca0c6904fa350cf78146255ed10ab772
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-26 21:56:04 -0500

    added code for JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,18 @@ var connection = mysql.createConnection({
 	database: "burgers_db",
 });
 
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "MiloLucas123!",
+		database: "burger_db",
+	});
+}
+
 // Make connection.
 connection.connect(function (err) {
 	if (err) {
