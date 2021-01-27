23788348afcadf2ddde609cd773173e332d2364f
Miranda McDonald <mirandagrace.dev@gmail.com>
2021-01-26 19:29:06 -0500

    structuring the project. added npm packages and connection to mysql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('The Original Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('BBQ Bacon Cheeseburger');
